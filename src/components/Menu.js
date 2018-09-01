27826b28ab13ed32f163851f2103ab85f39c2a29
Jaeyoon Song <jyo3on@gmail.com>
2018-09-02 00:01:50 +0900

    z index for bar

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -13,7 +13,7 @@ const Menu = () => {
             <ul className="tab-list">
                 <li className="tab"><NavLink exact to="/" activeStyle={activeStyle}>평가하기</NavLink></li>
                 <li className="tab"><NavLink exact to="/visited" activeStyle={activeStyle}>가봤어요</NavLink></li>
-                <li className="tab"><NavLink exact to="/unvisited" activeStyle={activeStyle}>안가봤어요</NavLink></li>
+                <li className="tab"><NavLink exact to="/unvisited" activeStyle={activeStyle}>안 가봤어요</NavLink></li>
             </ul>
         </div>
     );
