5576cd98e4df53f8a4931016d8280a780fb77ae7
Jaeyoon Song <jyo3on@gmail.com>
2018-09-05 12:03:10 +0900

    login API testing

--- src/shared/App.js
+++ src/shared/App.js
@@ -3,6 +3,11 @@ import { Route, Switch } from 'react-router-dom';
 import { Home, Login } from 'pages';
 
 class App extends Component {
+    componentDidMount() {
+        const serverAPI = "http://localhost:1323/";
+        localStorage.setItem('serverAPI', serverAPI);
+    }
+
     render() {
         return (
             <div>
--- src/store/configure.js
+++ src/store/configure.js
@@ -1,8 +1,32 @@
 import { createStore } from 'redux';
-import reducers from './reducers';
+
+const UPDATE_STAR = 'UPDATE_STAR';
+const UPDATE_REVIEW = 'UPDATE_REVIEW';
+
+const initialState = {
+  star: null,
+  review: ""
+};
+
+const reducer = (state = initialState, action) => {
+  switch(action.type) {
+    case UPDATE_STAR:
+      return { 
+        star: state.star
+      };
+    case UPDATE_REVIEW:
+      return { 
+        review: state.review
+      };
+    default:
+      return state;
+  }
+};
   
 const configureStore = (initialState) => {
-    const store = createStore(reducers);
+  
+    const store = createStore(reducer, initialState);
+  
     return store;
 };
 
